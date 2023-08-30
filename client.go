8b3e0d4d2dc88b47fd0ba5f50dcf3676d8ba925a
menaha-chandrasekar <menahachandrasekar@gmail.com>
2023-08-30 13:29:59 +0530

    changes

---
+++ client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	pb "module/netxd_customer/customer_proto"
+
+	"google.golang.org/grpc"
+	
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewCustomerServiceClient(conn)
+	
+
+	response, err := client.CreateCustomer(context.Background(), &pb.CustomerRequest{CustomerId: 107})
+	if err != nil {
+		log.Fatalf("Failed to call SayHello: %v", err)
+	}
+
+	fmt.Printf("Response: %d  %v\n", response.CustomerId,response.CreatedAt)
+}
